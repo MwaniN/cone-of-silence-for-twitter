d9271e1ca47ca56dbdb2e518d0f6d89ebc65868f
MwaniN <brooklynewyork@gmail.com>
2024-04-29 03:10:46 -0400

    Working method for the main timeline

--- scripts/content.js
+++ scripts/content.js
@@ -7,27 +7,10 @@ function removePostText(element) {
 
 let textParent = element.querySelectorAll(".css-1rynq56.r-8akbws.r-krxsd3.r-dnmrzs.r-1udh08x.r-bcqeeo.r-qvutc0.r-37j5jr.r-a023e6.r-rjixqe.r-16dba41.r-bnwqim")
 
-function recursion(input) {
-// be more specific and check for the class names or something to specify
-// those spans where text, hashtags, and links exist.
-// otherwise it looks like it's bonking the whole tweet
+// this works on the main page, will need to be different when it's on the individual tweet's page it seems
 
-  if (input.innerText) {
-    if (input.innerText.length > 0) {
-      input.innerText = '';
-    }
-  }
-
-  if (input.hasChildNodes()) {
-    let currChildren = input.childNodes;
-    for (let child of currChildren) {
-      recursion(child);
-    }
-  }
-
+textParent.remove();
+return
 
 }
 
-recursion(element);
-
-}
