ae3c5d3c170dc8435cb8b961063f2755202f12ad
MwaniN <brooklynewyork@gmail.com>
2024-04-29 20:04:43 -0400

    Notes

--- scripts/content.js
+++ scripts/content.js
@@ -1,18 +1,21 @@
 
 function removePostText(element) {
-// classes css-1rynq56 r-8akbws r-krxsd3 r-dnmrzs r-1udh08x r-bcqeeo r-qvutc0 r-37j5jr r-a023e6 r-rjixqe r-16dba41 r-bnwqim
 
-let textParent = element.querySelectorAll(".css-1rynq56.r-8akbws.r-krxsd3.r-dnmrzs.r-1udh08x.r-bcqeeo.r-qvutc0.r-37j5jr.r-a023e6.r-rjixqe.r-16dba41.r-bnwqim")
-let quotedTweetText = element.querySelectorAll(".css-1rynq56.r-8akbws.r-krxsd3.r-dnmrzs.r-1udh08x.r-bcqeeo.r-qvutc0.r-37j5jr.r-a023e6.r-rjixqe.r-16dba41.r-bnwqim.r-14gqq1x")
-
-// this works on the main page, will need to be different when it's on the individual tweet's page it seems
+let textParent = element.querySelectorAll(".css-1rynq56.r-8akbws.r-krxsd3.r-dnmrzs.r-1udh08x.r-bcqeeo.r-qvutc0.r-37j5jr.r-a023e6.r-rjixqe.r-16dba41.r-bnwqim");
+let quotedTweetText = element.querySelectorAll(".css-1rynq56.r-8akbws.r-krxsd3.r-dnmrzs.r-1udh08x.r-bcqeeo.r-qvutc0.r-37j5jr.r-a023e6.r-rjixqe.r-16dba41.r-bnwqim.r-14gqq1x");
+let actualTweetPageMainText =element.querySelectorAll(".css-1rynq56.r-bcqeeo.r-qvutc0.r-37j5jr.r-1inkyih.r-16dba41.r-bnwqim.r-135wba7");
 
 if (textParent.length > 0) {
   textParent[0].remove();
 }
 
 if (quotedTweetText.length > 0) {
+  // works for both the timeline and the main tweet page if quoted
   quotedTweetText[0].remove();
+}
+
+if (actualTweetPageMainText.length > 0) {
+  actualTweetPageMainText[0].remove();
 }
 
 return
@@ -34,3 +37,5 @@ removeAllPostText();
 // const observer = new MutationObserver(callbackHere)
 
 // mvp - remove all post text on main page and individual post link
+
+// tweets[1].querySelectorAll("[data-testid=videoComponent]") actually worked in the Console
